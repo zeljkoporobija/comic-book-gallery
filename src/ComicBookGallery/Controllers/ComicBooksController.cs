beeb56a0c17f9f67e6589eb53ff5c782b0565c1d
zeljk <zeljk@LAPTOP-N84QCSGL>
2016-08-11 10:23:57 +0200

    Added action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,7 +6,12 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+    public class ComicBooksController : Controller //we had to add directive using Syste.Web.Mvc; the class has to be public
     {
+        //Action method
+        public string Detail()
+        {
+            return "Hello from the Comic books controller";//we get again the error 404, but when we typed in the address bar http://localhost:60567/ComicBooks/Detail we see the message
+        }
     }
 }
