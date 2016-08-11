20a258b44623bc3ea4f1af42e9b70bf2c4add28f
zeljk <zeljk@LAPTOP-N84QCSGL>
2016-08-11 11:42:11 +0200

    Updated Detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,12 +6,25 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller //we had to add directive using Syste.Web.Mvc; the class has to be public
+    public class ComicBooksController : Controller 
+        //we had to add directive using Syste.Web.Mvc; the class has to be public and it needs to inherit ControllerBase Class
     {
-        //Action method
-        public string Detail()
+        //Action method - every public method on a controller
+        //public ContentResult Detail(), changed to
+        public ActionResult Detail()
         {
-            return "Hello from the Comic books controller";//we get again the error 404, but when we typed in the address bar http://localhost:60567/ComicBooks/Detail we see the message
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Thursday)//(today is Thursday, and by typing ComicBooks/Details we are redirected to the HomePage
+            {
+                //return new RedirectResult("/");//press CTRL+F12 to see its base class (ActionResult); now it can be replaced with
+                return Redirect("/");
+            }
+            /*instead of return string Detail()
+             * return "Hello from the Comic books controler";
+             * Yet, since we need to instantiate ContentResult Class, we write
+            */
+            //return new ContentResult()//CTRL+F12 -its base class is also ActionResult
+            //thus, in the end, we write:
+            return Content("Hello from the Comic books controller");
+           }
         }
-    }
 }
