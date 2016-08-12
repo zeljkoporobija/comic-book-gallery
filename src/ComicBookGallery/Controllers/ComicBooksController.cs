acf4761ddbbc7f98da80e729022251ced7c10d68
zeljk <zeljk@LAPTOP-N84QCSGL>
2016-08-12 11:34:15 +0200

    Updated Comic Book Detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,18 +14,34 @@ namespace ComicBookGallery.Controllers
         //public ContentResult Detail(), changed to
         public ActionResult Detail()
         {
-            //var seriesTitle = "The Amazing Spider-Man"; we put ViewBag object here and the variable's name is capitalized
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";//ViewBag is dynamic
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            //PASSING THE DATA TO VIEW W/OUT VIEWBAG - OBJECT INITIALIZER SYNTAX:
+            var comicBook = new ComicBook()//necessarily to press the light bulb and give directive use CommicBookGallery.Models
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Vicot Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
+
             };
+            //var seriesTitle = "The Amazing Spider-Man"; we put ViewBag object here and the variable's name is capitalized
+            //ViewBag.SeriesTitle = "The Amazing Spider-Man";//ViewBag is dynamic
+            //ViewBag.IssueNumber = 700;
+            //ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            //ViewBag.Artists = new string[]
+            //{
+            //    "Script: Dan Slott",
+            //    "Pencils: Humberto Ramos",
+            //    "Inks: Victor Olazaba",
+            //    "Colors: Edgar Delgado",
+            //    "Letters: Chris Eliopoulos"
+            //};
             /*if(DateTime.Today.DayOfWeek == DayOfWeek.Thursday)//(today is Thursday, and by typing ComicBooks/Details we are redirected to the HomePage
             {
                 //return new RedirectResult("/");//press CTRL+F12 to see its base class (ActionResult); now it can be replaced with
@@ -38,7 +55,7 @@ namespace ComicBookGallery.Controllers
             //thus, in the end, we write:
             //return Content("Hello from the Comic books controller");
             //since all of the code in { } belongs to View, the only thing we need to write here is:
-            return View();
+            return View(comicBook);
            }
         }
 }
