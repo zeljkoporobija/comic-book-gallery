7b6203e87d929e0cd87a2e369b2b2213d344db22
zeljk <zeljk@LAPTOP-N84QCSGL>
2016-08-13 12:21:23 +0200

    Updated comic books controller to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,15 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller 
         //we had to add directive using Syste.Web.Mvc; the class has to be public and it needs to inherit ControllerBase Class
     {
+        private ComicBookRepository _comicBookRepository = null; //naming convention for private fields
+
+        public ComicBooksController()//constructor - no return type; the name is like a class name
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
         //Action method - every public method on a controller
         //public ContentResult Detail(), changed to
-        public ActionResult Detail()
+        public ActionResult Detail(int? id)//int is set to nullable
         {
             //PASSING THE DATA TO VIEW W/OUT VIEWBAG - OBJECT INITIALIZER SYNTAX:
             //var comicBook = new ComicBook()//necessarily to press the light bulb and give directive use CommicBookGallery.Models
@@ -57,6 +64,13 @@ namespace ComicBookGallery.Controllers
             //thus, in the end, we write:
             //return Content("Hello from the Comic books controller");
             //since all of the code in { } belongs to View, the only thing we need to write here is:
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);//explicit cast; the other option is (id.Value)
+
             return View(comicBook);
            }
         }
